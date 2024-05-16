9b23cd54f48e1e67ae8fca958493b430760b47db
TheNathanSpace <nathan@thenathan.space>
2024-05-16 13:56:28 -0600

    works

--- reverse.py
+++ reverse.py
@@ -1,11 +1,9 @@
-import json
 import os
 import re
-from pathlib import Path
 
 import spotipy
 from dotenv import load_dotenv
-from spotipy import SpotifyClientCredentials, SpotifyOAuth
+from spotipy import SpotifyOAuth
 
 load_dotenv()
 
@@ -25,7 +23,7 @@ if __name__ == '__main__':
     CLIENT_SECRET = os.environ["CLIENT_SECRET"]
 
     playlist_url = input("Enter playlist URL: ")
-    matched = re.match(string=playlist_url, pattern=".*open\.spotify\.com\/playlist\/(.*)(\?(.*))?")
+    matched = re.match(string=playlist_url, pattern=".*open\.spotify\.com\/playlist\/([^?]*)")
     playlist_uri = "spotify:playlist:"
     if matched:
         playlist_uri += matched.group(1)
@@ -36,23 +34,12 @@ if __name__ == '__main__':
     scope = "playlist-read-private,playlist-read-collaborative,playlist-modify-private,playlist-modify-public"
     auth = SpotifyOAuth(scope=scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
                         redirect_uri="http://localhost:9090")
-    spotify = spotipy.Spotify(
-        auth_manager=auth)
-    token = auth.get_cached_token()
-    print(f"Token: {token['access_token']}")
+    spotify = spotipy.Spotify(auth_manager=auth)
     playlist = spotify.playlist(playlist_uri)
-    print(f"Start snapshot ID: {playlist['snapshot_id']}")
     playlist_tracks = get_playlist_tracks(spotify, playlist_uri)
     playlist_tracks = [track["track"]["name"] for track in playlist_tracks]
-    Path("playlist.json").write_text(json.dumps(playlist_tracks, indent=4))
     playlist_size = len(playlist_tracks)
 
-    result = spotify.playlist_reorder_items(playlist_id=playlist_uri, range_start=playlist_size - 1, insert_before=0,
-                                            range_length=1, snapshot_id=playlist["snapshot_id"])
-    print(f"End snapshot ID:   {result['snapshot_id']}")
-    exit()
-
     for i in range(0, playlist_size):
-        print(f"Moving {playlist_size - i} to before {0}")
-        spotify.playlist_reorder_items(playlist_id=playlist_uri, range_start=playlist_size - 1, insert_before=0,
+        spotify.playlist_reorder_items(playlist_id=playlist_uri, range_start=playlist_size - 1, insert_before=i,
                                        range_length=1)
